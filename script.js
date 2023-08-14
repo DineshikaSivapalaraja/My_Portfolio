6160fa0d808d8b57481b1e9c15b9d027af719477
Dineshika <91914577+DineshikaSivapalaraja@users.noreply.github.com>
2023-08-14 15:39:02 +0530

    Minor correction

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { /*Academic section Toggling */
     const academicSec = document.getElementById("academic");
     const academicLink = document.querySelector('a[href="#academic"]');
   
@@ -11,17 +11,24 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () { /*Extra curricular section Toggling */
   const activitiesSec = document.getElementById("activities");
   const activitiesLink = document.querySelector('a[href="#activities"]');
 
   activitiesLink.addEventListener("click", function (event) {
     event.preventDefault();
     activitiesSec.classList.toggle("show");
-    if(activitiesSec.classList.contains("show")) {  //adding smooth scrolling after toggling
+    if(activitiesSec.classList.contains("show")) {  //Adding smooth scrolling after toggling
       activitiesSec.scrollIntoView({behavior: "smooth"});
     }
   });
 });
-  
+
+const navToggle = document.getElementById("navToggle");  /*Navigation bar */
+const navLinks = document.getElementById("navLinks");
+
+navToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
+
 
